c92daf057f496fc3553e02fa3923f7a40caba38f
SWARANJIT <166933500+devswaranjit-codes@users.noreply.github.com>
2025-09-11 00:58:28 +0530

    Create file_organizer.sh

---
+++ file_organizer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+echo "Enter the directory path to organize:"
+read DIR
+
+if [[ ! -d "$DIR" ]];
+then
+    echo "-X-X-X-X- Directory does not exist! -X-X-X-X"
+    exit 1
+fi
+
+
+cd "$DIR"
+
+
+for file in *; do
+    if [[ -f "$file" ]]; 
+then
+        
+        ext="${file##*.}"
+        ext=$(echo "$ext" | tr '[:upper:]' '[:lower:]')
+
+        
+        if [[ "$file" == "$ext" ]]; then
+            continue
+        fi
+
+        case "$ext" in
+            jpg|jpeg|png|gif) folder="Images" ;;
+            mp3|wav|flac)     folder="Music" ;;
+            mp4|mkv|mov|avi)  folder="Videos" ;;
+            pdf|txt) folder="Documents" ;;
+	    doc|docx) folder="word documents";;
+	    pptx) folder="presentations";;
+            zip|tar|gz|rar)   folder="Archives" ;;
+            *)                folder="Others" ;;
+        esac
+
+        
+        mkdir -p "$folder"
+
+        
+        mv "$file" "$folder/"
+        echo "Moved $file → $folder/"
+    fi
+done
+
+echo ".X.X.X.X.X. Organization complete! .X.X.X.X.X"
